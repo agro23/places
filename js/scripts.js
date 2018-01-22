e82482727b475d1f9d5660e3a41aeb52f4cfd82b
Andy Grossberg and Michael Albers <andy@>
2018-01-22 09:54:34 -0800

    make array of location objects

--- js/scripts.js
+++ js/scripts.js
@@ -12,11 +12,19 @@ function Place(name, visited, date, landmark, season, note) { // Place construct
 
 
 function testCity(){ // make, fill and show a place
-  var city = new Place("SF", false, "11/12/02", ["Coit Tower", "Bridge", "Twin Peaks"], "Fall", "Too many brogrammers.");
-  console.log("Name 1 = " + city.name);
-  // city = new Place;
-  // city.name = "New Orleans";
-  // console.log("Name 2 = " + city.name);
+  // var myPlace = new Place("SF", false, "11/12/02", ["Coit Tower", "Bridge", "Twin Peaks"], "Fall", "Too many brogrammers.");
+  // console.log("Name 1 = " + myPlace.name);
+  // var myPlace = new Place("New Orleans", false, "01/01/10", ["French Quarter", "Jazz Club"], "Winter", "Lots of drunks.");
+  // console.log("Name 2 = " + myPlace.name);
+
+  locations = [];
+  var myPlace = new Place("SF", false, "11/12/02", ["Coit Tower", "Bridge", "Twin Peaks"], "Fall", "Too many brogrammers.");
+  locations.push(myPlace);
+  var myPlace = new Place("New Orleans", false, "01/01/10", ["French Quarter", "Jazz Club"], "Winter", "Lots of drunks.");
+  locations.push(myPlace);
+  console.log("Name 2 = " + locations[1].name);
+  console.log(locations);
+
 }
 
 

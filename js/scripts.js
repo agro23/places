53e6dd776726f8d3199c03844e6aff77839ce572
Andy Grossberg and Michael Albers <andy@>
2018-01-22 09:07:36 -0800

    add Place object constructor

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+// Business Logic
+function Place(name, visited, date, landmark, season, note)= {
+  this.name = name;
+  this.visited = visited;
+  this.date = date;
+  this.landmark = landmark;
+  this.season = season;
+  this.note = note;
+};
+
+$(document).ready(function()); {
+  $('form#place').submit(event){
+    event.preventDefault();
+    
+  }
+});
